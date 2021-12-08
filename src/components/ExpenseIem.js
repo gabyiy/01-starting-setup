533eba64bcd0c94b7ebefe700c2c360d350686d4
Nita Gabriel <62211186+gabyiy@users.noreply.github.com>
2021-12-08 19:48:15 +0100

    next

--- src/components/ExpenseIem.js
+++ src/components/ExpenseIem.js
@@ -1,16 +1,17 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   const expenseDate = new Date(2020, 11, 25);
   const expenseTitle = "Car insurance";
   const expenseAmount = 284.67;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <div className="expense-item__description">{expenseTitle}</div>
-      <div className="expense-item__price">{expenseAmount}</div>
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">{props.title}</div>
+      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
+  //si aici  cu parametru props av em acces la ce avem salavat in expensive in app.js
 }
 
 export default ExpenseItem;
